eb47a4f899b187984c388133dd9c88e3fe6e6111
Casey-IoT <sctabone@iotsolutions.com.mt>
2026-02-05 12:57:13 +0100

    Added Code to pass HTML elements as a result to the client

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const http = require('http');
 //Create Server Logic that runs for each request
 const server = http.createServer((req,res) => {
     console.log(req.url, req.method, req.headers);
+    
+    //Pass Meta Info that the type of return content will be HTML
+    res.setHeader('Content-Type', 'text/html');
+
+    //Write to Result
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+
+    //Mark end of result
+    res.end();
 
     //Exit the process
     //process.exit();
