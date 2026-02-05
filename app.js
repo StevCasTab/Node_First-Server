cffcc04e93ae1007ca66551e23ae8e6ac9808361
Casey-IoT <sctabone@iotsolutions.com.mt>
2026-02-05 13:11:52 +0100

    Added Logic to take User input, write a dummy message text file, and redirect user back to form

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 //import HTTP Library
 const http = require('http');
 
+//import file system library
+const fs = require('fs');
+
 //Create Server Logic that runs for each request
 const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
     
-    //Pass Meta Info that the type of return content will be HTML
-    res.setHeader('Content-Type', 'text/html');
+    const url = req.url;
+    const method = req.method;
 
-    //Write to Result
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
+    if(url === '/')
+    {
+        //Write to Result
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></body>');
+        res.write('</html>');
+    }
+    else if(url === "/message" && method === "POST")
+    {
+        //Create new file
+        fs.writeFileSync('message.txt', 'DUMMY');
 
+        //Pass header 302 (Redirection)
+        res.statusCode = 302;
+
+        //Set Header of result to redirect to new URL
+        res.setHeader('Location', '/');
+
+    }
+    else{
+        //Write to Result
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+        res.write('</html>');
+    }
     //Mark end of result
     res.end();
 
