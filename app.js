6802ef9d43f5d45225de65c4f241cb203e08d2f9
Casey-IoT <sctabone@iotsolutions.com.mt>
2026-02-05 12:43:46 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//import HTTP Library
+const http = require('http');
+
+//Create Server Logic that runs for each request
+const server = http.createServer((req,res) => {
+    console.log(req);
+
+    //Exit the process
+    process.exit();
+});
+
+//Listen on Port 3000 which is safe for localhost usage
+server.listen(3000);
+
+
+
+
+
+
+// /*function rqListener(req, res)
+// {
+//     console.log(req);
+// }
+
+// http.createServer(rqListener);*/
+
+
+//========================================================
+//OR
+//========================================================
+/*http.createServer(function(req, res) {
+
+});*/
+
+//========================================================
+
+
+
+//========================================================
+//OR
+//========================================================
+
+/*http.createServer((req,res) => {
+    
+});*/
+
+//========================================================
